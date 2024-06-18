8f82621e8a1657a12804c9f31ba2ec0ace4c060b
sabhis231 <sabhis231@gmail.com>
2024-06-19 00:30:35 +0100

    new setup with configuration

--- GA-Alert.py
+++ GA-Alert.py
@@ -19,6 +19,7 @@ from google.analytics.data_v1beta.types import (
 
 
 def _load_global_values():
+    print("Start of _load_global_values >>>")
 
     global MINUTES_AGO
     global MINUTES
@@ -29,10 +30,20 @@ def _load_global_values():
 
     global WEBSITE_URL
 
+    global DAY
+    global HOUR
+
+    global _TRUE
+
     MINUTES_AGO = "minutesAgo" # this is defult value if value is not specified in ApplicationData file
     MINUTES = "02" # this is defult value if value is not specified in ApplicationData file
     PROPERTY_DIMENSION = "unifiedScreenName" # this is defult value if value is not specified in ApplicationData file
     PROPERTY_METRIC = "screenPageViews" # this is defult value if value is not specified in ApplicationData file
+
+    HOUR = int(datetime.now().strftime("%H"))
+    DAY = datetime.now().strftime("%A")
+
+    _TRUE = True
 
     f = open(
         "ApplicationData.json",
@@ -42,33 +53,15 @@ def _load_global_values():
     WEBSITE_URL = "https://www.google.com/analytics/web/"
 
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
+    print("End of _load_global_values <<<")
 
 
 def _get_thresold_traffic_data(reqPropertFilter):
     print("Start of _get_thresold_traffic_data >>>")
-    print("reqPropertFilter = ", reqPropertFilter)
-    hour = int(datetime.now().strftime("%H"))
-    day = datetime.now().strftime("%A")
-    for thresoldLimitDays in reqPropertFilter["thresoldLimit"]:
-        if day.lower() in thresoldLimitDays.lower():
-            for thresoldLimitDaysHous in reqPropertFilter["thresoldLimit"][
-                thresoldLimitDays
-            ]:
-                if hour >= int(thresoldLimitDaysHous.split("-")[0]) and hour < int(
-                    thresoldLimitDaysHous.split("-")[1]
-                ):
-                    print("thresoldLimitDays = ", thresoldLimitDays)
-                    print("thresoldLimitDaysHous = ", thresoldLimitDaysHous)
-                    print(
-                        "thresoldLimitDaysHousLimt = ",
-                        reqPropertFilter["thresoldLimit"][thresoldLimitDays][
-                            thresoldLimitDaysHous
-                        ],
-                    )
-                    print("End of _get_thresold_traffic_data <<<")
-                    return reqPropertFilter["thresoldLimit"][thresoldLimitDays][
-                        thresoldLimitDaysHous
-                    ]
+    for thresoldLimitDaysHous in reqPropertFilter["thresoldLimit"]:
+        if HOUR >= int(thresoldLimitDaysHous.split("-")[0]) and HOUR < int(thresoldLimitDaysHous.split("-")[1]):
+            print("End of _get_thresold_traffic_data <<<")
+            return reqPropertFilter["thresoldLimit"][thresoldLimitDaysHous]
 
 def _run_dynamic_query():
     emailMasterContent = dict()
@@ -76,57 +69,38 @@ def _run_dynamic_query():
     print(APPLICATION_DATA)
     day = datetime.now().strftime("%A")
     for application in APPLICATION_DATA:
-        for emailConfigDays in application["emailConfig"]:
-            print("emailConfigDays = ", emailConfigDays)
-            if day.lower() in emailConfigDays.lower():
-                print("emailConfigDayssss = ", application["emailConfig"][emailConfigDays]["isEmail"])
-                if application["emailConfig"][emailConfigDays]["isEmail"] == True:
-                    emailData = _process_report(
-                        application["propertyId"],
-                        application["propertyName"],
-                        application["trafficThresoldLimit"],
-                        application["propertyThresold"],
-                    )
+        for appKeys in application:
+            if day.lower() in appKeys.lower():
+                appData = application[appKeys]
+                isEnabled = application[appKeys]['isEnabled'] if "isEnabled" in application[appKeys].keys() else _TRUE
+                if isEnabled:
+                    emailData = _process_report(application["propertyId"], application["propertyName"], appData)
                     emailMasterContent = _dict_add(
                         emailMasterContent, application["propertyName"], emailData
                     )
         print(
             "******************************************************************************"
-        )
+        )  
     print("End of _run_dynamic_query <<<")
     return emailMasterContent
 
 
-def _process_report(propertyId, propertyName, propertFilter, propertyThresold):
-    # print("Start of _process_report >>>")
+def _process_report(propertyId, propertyName, appData):
     return _report_based_on_filter(
-        propertyId, propertyName, propertFilter, propertyThresold
+        propertyId, propertyName, appData
     )
-    # _check_website()
-    print("End of _process_report <<<")
 
 
-def _report_based_on_filter(
-    propertyId, propertyName, propertFilter, propertyThresold
-):
+def _report_based_on_filter(propertyId, propertyName, appData):
     print("Start of _report_based_on_filter >>>")
-    print("propertyId = ", propertyId)
-    print("propertyName = ", propertyName)
-    print("propertFilter = ", propertFilter)
-    print("propertyThresold = ", propertyThresold)
-    hour = int(datetime.now().strftime("%H"))
-    minutes = 0
-    dimension = PROPERTY_DIMENSION
-    metric = PROPERTY_METRIC
-    day = datetime.now().strftime("%A")
-    for propertyThresoldData in propertyThresold:
-        print("propertyThresoldData = ", propertyThresoldData)
-        if day.lower() in propertyThresoldData.lower():
-            print(propertyThresold[propertyThresoldData])
-            minutes = propertyThresold[propertyThresoldData]["propertyMinutes"] if "propertyMinutes" in propertyThresold[propertyThresoldData].keys() else MINUTES
-            dimension = propertyThresold[propertyThresoldData]["propertyDimension"] if "propertyDimension" in propertyThresold[propertyThresoldData].keys() else PROPERTY_DIMENSION
-            metric = propertyThresold[propertyThresoldData]["propertyMetric"] if "propertyMetric" in propertyThresold[propertyThresoldData].keys() else PROPERTY_METRIC
-    print("minutes = ", minutes)
+    if  "propertyThresold" in appData.keys():
+        minutes = appData["propertyThresold"]["propertyMinutes"] if "propertyMinutes" in appData["propertyThresold"].keys() else MINUTES
+        dimension = appData["propertyThresold"]["propertyDimension"] if "propertyDimension" in appData["propertyThresold"].keys() else PROPERTY_DIMENSION
+        metric = appData["propertyThresold"]["propertyMetric"] if "propertyMetric" in appData["propertyThresold"].keys() else PROPERTY_METRIC
+    else:
+        minutes = MINUTES
+        dimension = PROPERTY_DIMENSION
+        metric = PROPERTY_METRIC
     client = BetaAnalyticsDataClient()
     request = RunRealtimeReportRequest(
         property=f"properties/{propertyId}",
@@ -157,59 +131,39 @@ def _report_based_on_filter(
         ),
         metrics=[Metric(name=metric)],
     )
-    print(request)
     response = client.run_realtime_report(request)
     print("End of _report_based_on_filter >>>")
-    return _process_result(propertyName, propertFilter, response)
+    return _process_result(propertyName, appData, response)
 
 
-def _process_result(propertyName, propertFilters, response):
+def _process_result(propertyName, appData, response):
     print("Start of _process_result >>>")
     emailData = dict()
-    
-    print("response = ", response)
-    for filters in propertFilters:
-        print("filters = ", filters)
-        thresoldValue = _get_thresold_traffic_data(propertFilters[filters])
-        print("thresoldValue = ", thresoldValue)
-        print("propertFilters[filters] = ", propertFilters[filters])
+    trafficThresoldLimit = appData["trafficThresoldLimit"] if "trafficThresoldLimit" in appData.keys() else {}
+    for filters in trafficThresoldLimit:
+        thresoldValue = _get_thresold_traffic_data(trafficThresoldLimit[filters])
         if thresoldValue is not None:
             isLessThanCheck = False
-            if propertFilters[filters]["check"] == "<":
+            lessOrGreater = trafficThresoldLimit[filters]["check"]
+            if lessOrGreater == "<":
                 isLessThanCheck = True
-            print("isLessThanCheck = ", isLessThanCheck)
             for filter in filters.split(","):
-                print("filter = ", filter)
                 isGreaterThanCheck = False
                 isDimensionMatched = False
                 isMetricCheck =  False
                 screenEmailData = dict()
                 for row in response.rows:
-                    print("row = ", row)
                     if isLessThanCheck:
                         if filter.lower() in row.dimension_values[0].value.lower():
                             isDimensionMatched = True
                             if int(row.metric_values[0].value) > int(thresoldValue):
-                                print("")
-                                print(
-                                    "isLessThanCheck TRUE = ",
-                                    row.dimension_values[0].value
-                                    + "  | = "
-                                    + row.metric_values[0].value,
-                                )
-                                screenEmailData= _dict_add(screenEmailData, 'actual',thresoldValue)
-                                screenEmailData= _dict_add(screenEmailData, 'current',row.metric_values[0].value)
-                                # print("screenEmailData = ", screenEmailData)
-                                emailData = _dict_add(
-                                    emailData,
-                                    filter,
-                                    screenEmailData,
-                                )
+                                screenEmailData = _dict_add(screenEmailData, 'actual',thresoldValue)
+                                screenEmailData = _dict_add(screenEmailData, 'current',row.metric_values[0].value)
+                                emailData = _dict_add(emailData, filter, screenEmailData)
                             else:
                                 isMetricCheck = True
                                 print("int(row.metric_values[0].value) = ", int(row.metric_values[0].value))
                                 print("thresoldValue = ", thresoldValue)
-                            # isGreaterThanCheck = True
                             break
                         else:
                             print("isLessThanCheck TRUE filter.lower() = ", filter.lower())
@@ -217,110 +171,65 @@ def _process_result(propertyName, propertFilters, response):
                     else:
                         if filter.lower() in row.dimension_values[0].value.lower():
                             isDimensionMatched = True
-                            # isMetricCheck = True
                             if int(row.metric_values[0].value) < int(thresoldValue):
-                                print(
-                                    "isLessThanCheck Flase = ",
-                                    row.dimension_values[0].value
-                                    + "  = = "
-                                    + row.metric_values[0].value,
-                                )
                                 screenEmailData= _dict_add(screenEmailData, 'actual',thresoldValue)
                                 screenEmailData= _dict_add(screenEmailData, 'current',row.metric_values[0].value)
-                                print("screenEmailData = ",screenEmailData)
-                                emailData = _dict_add(
-                                    emailData,
-                                    filter,
-                                    screenEmailData,
-                                )
+                                emailData = _dict_add(emailData, filter, screenEmailData)
                             else:
                                 isMetricCheck = True
                                 print("int(row.metric_values[0].value) = ", int(row.metric_values[0].value))
                                 print("thresoldValue = ", thresoldValue)
-                            # isGreaterThanCheck = True
                             break
                         else:
                             print("isLessThanCheck FALSE filter.lower() = ", filter.lower())
                             print("isLessThanCheck FALSE ow.dimension_values[0].value.lower() = ", row.dimension_values[0].value.lower())
-
-                print("isDimensionMatched = ", isDimensionMatched)
-                print("isLessThanCheck = ", isMetricCheck)
                 if isDimensionMatched is False:
                     if isLessThanCheck is False:
-                        screenEmailData= _dict_add(screenEmailData, 'actual', thresoldValue)
-                        screenEmailData= _dict_add(screenEmailData, 'current', '0')
-                        print("screenEmailData = ",screenEmailData)
-                        emailData = _dict_add(
-                            emailData,
-                            filter,
-                            screenEmailData,
-                        ) 
-                # if isDimensionMatched:
-                #     if isMetricCheck:
-                #         print("isDimensionMatched = ", isDimensionMatched)
-                #         print("isMetricCheck = ", isMetricCheck)
-                # else:
-                #     if isLessThanCheck is False:
-                #         print("237 isDimensionMatched = ", isDimensionMatched)
-                #         print("238 isMetricCheck = ", isMetricCheck)
-                #         screenEmailData= _dict_add(screenEmailData, 'actual',thresoldValue)
-                #         screenEmailData= _dict_add(screenEmailData, 'current','0')
-                #         print("screenEmailData = ",screenEmailData)
-                #         emailData = _dict_add(
-                #             emailData,
-                #             filter,
-                #             screenEmailData,
-                #         )
-                
-    print("emailData = ", emailData)
+                        screenEmailData = _dict_add(screenEmailData, 'actual', thresoldValue)
+                        screenEmailData = _dict_add(screenEmailData, 'current', '0')
+                        emailData = _dict_add(emailData, filter, screenEmailData)
     print("End of _process_result <<<")
     return emailData
 
 def _send_email(messageData, appName, emailConfig):
     print("Start of _send_email >>>")
-    message = MIMEMultipart("alternative")
-    message["Subject"] = "[Important] Alert for " + appName 
-    message["From"] = emailConfig["smtpUserName"]
-    part = MIMEText(messageData, "html")
-    message.attach(part)
-    conn = smtplib.SMTP(emailConfig["smtpChannel"], emailConfig["smtpPort"])
-    conn.ehlo()
-    conn.starttls()
-    conn.login(emailConfig["smtpUserName"], emailConfig["smtpUserKey"])
-    conn.sendmail(emailConfig["emailFrom"], emailConfig["emailTo"], message.as_string())
-    conn.quit()
+    isEmail = emailConfig['isEmail'] if "isEmail" in emailConfig.keys() else True
+    if isEmail:
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "[Important] Alert for " + appName 
+        message["From"] = emailConfig["smtpUserName"]
+        part = MIMEText(messageData, "html")
+        message.attach(part)
+        conn = smtplib.SMTP(emailConfig["smtpChannel"], emailConfig["smtpPort"])
+        conn.ehlo()
+        conn.starttls()
+        conn.login(emailConfig["smtpUserName"], emailConfig["smtpUserKey"])
+        conn.sendmail(emailConfig["emailFrom"], emailConfig["emailTo"], message.as_string())
+        conn.quit()
+    else:
+        print("isEmail = isEmail is false email not triggered")
+
+    
     print("End of _send_email <<<")
 
 
 def _dict_add(dictionary, key, value):
     print("Start of _dict_add >>>")
-    print("dictionary = ", dictionary)
-    print("key = ", key)
-    print("value = ", value)
     temp = dictionary.copy()
     temp[key] = value
-    print("temp = ", temp)
     print("End of _dict_add <<<")
     return temp
 
 
 def _draft_email(emailMasterContent):
     print("Start of _draft_email >>>")
-    print("emailMasterContent = ", emailMasterContent)
-    isSendEmail = False
     
     for application in APPLICATION_DATA:
         html = ""
         tablehtml = ""
         table = ""
         if application["propertyName"] in emailMasterContent.keys():
-            print("emailMasterContent[application[propertyName] = ", emailMasterContent[application["propertyName"]])
-            for index, emailData in enumerate(
-                emailMasterContent[application["propertyName"]]
-            ):
-                isSendEmail = True
-                print("emailData = ", emailData)
-                print(emailMasterContent[application["propertyName"]][emailData])
+            for index, emailData in enumerate(emailMasterContent[application["propertyName"]]):
                 if index % 2:
                     table += (
                         '<tr><td style="border: 1px solid #dddddd; text-align: left;padding: 8px;">'
@@ -372,17 +281,11 @@ def _draft_email(emailMasterContent):
             </body></html>
             """
             )
-            print("application[propertyName] = ", application["emailConfig"])
-            day = datetime.now().strftime("%A")
-            for emailConfigDays in application["emailConfig"]:
-                print("emailConfigDays = ", emailConfigDays)
-                if day.lower() in emailConfigDays.lower():
-                    print("application[emailConfig] = ", application["emailConfig"][emailConfigDays])
-                    if isSendEmail:
-                        _send_email(html, application["propertyName"], application["emailConfig"][emailConfigDays])
-                    else:
-                        print("isSendEmail = False Emai not triggered")
-
+            for appKey in application:
+                if DAY.lower() in appKey.lower():
+                    emailConfig = application[appKey]['emailConfig'] if "emailConfig" in application[appKey].keys() else None
+                    if emailConfig is not None:
+                        _send_email(html, application["propertyName"], emailConfig)
     print("End of _draft_email <<<")
 
 
@@ -394,6 +297,8 @@ def _check_website():
 
 
 if __name__ == "__main__":
+    print("Start of __main__  at >>> ", datetime.now())
     _load_global_values()
     emailMasterContent = _run_dynamic_query()
     _draft_email(emailMasterContent)
+    print("End of __main__ at >>>", datetime.now())

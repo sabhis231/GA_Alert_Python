2fb26f2e515ba76b3d2b1df9ebf5de6f8a019d78
sabhis231 <sabhis231@gmail.com>
2024-06-18 21:47:26 +0100

    Email Setup

--- GA-Alert.py
+++ GA-Alert.py
@@ -21,33 +21,24 @@ from google.analytics.data_v1beta.types import (
 def _load_global_values():
 
     global MINUTES_AGO
-    global UNIFIED_SCREEN_NAME
-    global SCREEN_PAGE_VIEWS
+    global MINUTES
+    global PROPERTY_DIMENSION
+    global PROPERTY_METRIC
 
     global APPLICATION_DATA
 
-    global SMTP_CHANNEL
-    global SMTP_PORT
-    global SMTP_USER_NAME
-    global SMTP_USER_KEY
-    global EMAIL_SENT_TO
-
     global WEBSITE_URL
 
-    MINUTES_AGO = "minutesAgo"
-    UNIFIED_SCREEN_NAME = "unifiedScreenName"
-    SCREEN_PAGE_VIEWS = "screenPageViews"
+    MINUTES_AGO = "minutesAgo" # this is defult value if value is not specified in ApplicationData file
+    MINUTES = "02" # this is defult value if value is not specified in ApplicationData file
+    PROPERTY_DIMENSION = "unifiedScreenName" # this is defult value if value is not specified in ApplicationData file
+    PROPERTY_METRIC = "screenPageViews" # this is defult value if value is not specified in ApplicationData file
 
     f = open(
         "ApplicationData.json",
     )
     APPLICATION_DATA = json.load(f)
-    SMTP_CHANNEL = "smtp.gmail.com"
-    SMTP_PORT = "587"
-    SMTP_USER_NAME = "xyz@gmail.com"
-    SMTP_USER_KEY = "abc cde efg ghj"
-    EMAIL_SENT_TO = ["xyz@gmail.com","abc@gmail.com"]
-
+    
     WEBSITE_URL = "https://www.google.com/analytics/web/"
 
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
@@ -83,16 +74,22 @@ def _run_dynamic_query():
     emailMasterContent = dict()
     print("Start of _run_dynamic_query >>>")
     print(APPLICATION_DATA)
+    day = datetime.now().strftime("%A")
     for application in APPLICATION_DATA:
-        emailData = _process_report(
-            application["propertyId"],
-            application["propertyName"],
-            application["trafficThresoldLimit"],
-            application["propertyThresoldMinutes"],
-        )
-        emailMasterContent = _dict_add(
-            emailMasterContent, application["propertyName"], emailData
-        )
+        for emailConfigDays in application["emailConfig"]:
+            print("emailConfigDays = ", emailConfigDays)
+            if day.lower() in emailConfigDays.lower():
+                print("emailConfigDayssss = ", application["emailConfig"][emailConfigDays]["isEmail"])
+                if application["emailConfig"][emailConfigDays]["isEmail"] == True:
+                    emailData = _process_report(
+                        application["propertyId"],
+                        application["propertyName"],
+                        application["trafficThresoldLimit"],
+                        application["propertyThresold"],
+                    )
+                    emailMasterContent = _dict_add(
+                        emailMasterContent, application["propertyName"], emailData
+                    )
         print(
             "******************************************************************************"
         )
@@ -100,36 +97,40 @@ def _run_dynamic_query():
     return emailMasterContent
 
 
-def _process_report(propertyId, propertyName, propertFilter, propertyThresoldMinutes):
-    print("Start of _process_report >>>")
+def _process_report(propertyId, propertyName, propertFilter, propertyThresold):
+    # print("Start of _process_report >>>")
     return _report_based_on_filter(
-        propertyId, propertyName, propertFilter, propertyThresoldMinutes
+        propertyId, propertyName, propertFilter, propertyThresold
     )
     # _check_website()
     print("End of _process_report <<<")
 
 
 def _report_based_on_filter(
-    propertyId, propertyName, propertFilter, propertyThresoldMinutes
+    propertyId, propertyName, propertFilter, propertyThresold
 ):
     print("Start of _report_based_on_filter >>>")
     print("propertyId = ", propertyId)
     print("propertyName = ", propertyName)
     print("propertFilter = ", propertFilter)
-    print("propertyThresoldMinutes = ", propertyThresoldMinutes)
+    print("propertyThresold = ", propertyThresold)
     hour = int(datetime.now().strftime("%H"))
     minutes = 0
+    dimension = PROPERTY_DIMENSION
+    metric = PROPERTY_METRIC
     day = datetime.now().strftime("%A")
-    for propertyThresoldMinute in propertyThresoldMinutes:
-        print("propertyThresoldMinute = ", propertyThresoldMinute)
-        if day.lower() in propertyThresoldMinute.lower():
-            print(propertyThresoldMinutes[propertyThresoldMinute])
-            minutes = propertyThresoldMinutes[propertyThresoldMinute]["propertyMinutes"]
+    for propertyThresoldData in propertyThresold:
+        print("propertyThresoldData = ", propertyThresoldData)
+        if day.lower() in propertyThresoldData.lower():
+            print(propertyThresold[propertyThresoldData])
+            minutes = propertyThresold[propertyThresoldData]["propertyMinutes"] if "propertyMinutes" in propertyThresold[propertyThresoldData].keys() else MINUTES
+            dimension = propertyThresold[propertyThresoldData]["propertyDimension"] if "propertyDimension" in propertyThresold[propertyThresoldData].keys() else PROPERTY_DIMENSION
+            metric = propertyThresold[propertyThresoldData]["propertyMetric"] if "propertyMetric" in propertyThresold[propertyThresoldData].keys() else PROPERTY_METRIC
     print("minutes = ", minutes)
     client = BetaAnalyticsDataClient()
     request = RunRealtimeReportRequest(
         property=f"properties/{propertyId}",
-        dimensions=[Dimension(name=UNIFIED_SCREEN_NAME)],
+        dimensions=[Dimension(name=dimension)],
         dimension_filter=FilterExpression(
             and_group=FilterExpressionList(
                 expressions=[
@@ -154,12 +155,12 @@ def _report_based_on_filter(
                 ]
             )
         ),
-        metrics=[Metric(name=SCREEN_PAGE_VIEWS)],
+        metrics=[Metric(name=metric)],
     )
     print(request)
     response = client.run_realtime_report(request)
-    return _process_result(propertyName, propertFilter, response)
     print("End of _report_based_on_filter >>>")
+    return _process_result(propertyName, propertFilter, response)
 
 
 def _process_result(propertyName, propertFilters, response):
@@ -189,6 +190,7 @@ def _process_result(propertyName, propertFilters, response):
                         if filter.lower() in row.dimension_values[0].value.lower():
                             isDimensionMatched = True
                             if int(row.metric_values[0].value) > int(thresoldValue):
+                                print("")
                                 print(
                                     "isLessThanCheck TRUE = ",
                                     row.dimension_values[0].value
@@ -240,38 +242,52 @@ def _process_result(propertyName, propertFilters, response):
                         else:
                             print("isLessThanCheck FALSE filter.lower() = ", filter.lower())
                             print("isLessThanCheck FALSE ow.dimension_values[0].value.lower() = ", row.dimension_values[0].value.lower())
-                if isDimensionMatched:
-                    if isMetricCheck:
-                        print("234 isDimensionMatched = ", isDimensionMatched)
-                        print("235 isMetricCheck = ", isMetricCheck)
-                else:
+
+                print("isDimensionMatched = ", isDimensionMatched)
+                print("isLessThanCheck = ", isMetricCheck)
+                if isDimensionMatched is False:
                     if isLessThanCheck is False:
-                        print("237 isDimensionMatched = ", isDimensionMatched)
-                        print("238 isMetricCheck = ", isMetricCheck)
-                        screenEmailData= _dict_add(screenEmailData, 'actual',thresoldValue)
-                        screenEmailData= _dict_add(screenEmailData, 'current','0')
+                        screenEmailData= _dict_add(screenEmailData, 'actual', thresoldValue)
+                        screenEmailData= _dict_add(screenEmailData, 'current', '0')
                         print("screenEmailData = ",screenEmailData)
                         emailData = _dict_add(
                             emailData,
                             filter,
                             screenEmailData,
-                        )
+                        ) 
+                # if isDimensionMatched:
+                #     if isMetricCheck:
+                #         print("isDimensionMatched = ", isDimensionMatched)
+                #         print("isMetricCheck = ", isMetricCheck)
+                # else:
+                #     if isLessThanCheck is False:
+                #         print("237 isDimensionMatched = ", isDimensionMatched)
+                #         print("238 isMetricCheck = ", isMetricCheck)
+                #         screenEmailData= _dict_add(screenEmailData, 'actual',thresoldValue)
+                #         screenEmailData= _dict_add(screenEmailData, 'current','0')
+                #         print("screenEmailData = ",screenEmailData)
+                #         emailData = _dict_add(
+                #             emailData,
+                #             filter,
+                #             screenEmailData,
+                #         )
+                
     print("emailData = ", emailData)
     print("End of _process_result <<<")
     return emailData
 
-def _send_email(messageData):
+def _send_email(messageData, appName, emailConfig):
     print("Start of _send_email >>>")
     message = MIMEMultipart("alternative")
-    message["Subject"] = "[Important] Alert for UI Apps"
-    message["From"] = SMTP_USER_NAME
+    message["Subject"] = "[Important] Alert for " + appName 
+    message["From"] = emailConfig["smtpUserName"]
     part = MIMEText(messageData, "html")
     message.attach(part)
-    conn = smtplib.SMTP(SMTP_CHANNEL, SMTP_PORT)
+    conn = smtplib.SMTP(emailConfig["smtpChannel"], emailConfig["smtpPort"])
     conn.ehlo()
     conn.starttls()
-    conn.login(SMTP_USER_NAME, SMTP_USER_KEY)
-    conn.sendmail(SMTP_USER_NAME, EMAIL_SENT_TO, message.as_string())
+    conn.login(emailConfig["smtpUserName"], emailConfig["smtpUserKey"])
+    conn.sendmail(emailConfig["emailFrom"], emailConfig["emailTo"], message.as_string())
     conn.quit()
     print("End of _send_email <<<")
 
@@ -292,70 +308,81 @@ def _draft_email(emailMasterContent):
     print("Start of _draft_email >>>")
     print("emailMasterContent = ", emailMasterContent)
     isSendEmail = False
-    tablehtml = ""
+    
     for application in APPLICATION_DATA:
+        html = ""
+        tablehtml = ""
         table = ""
-        for index, emailData in enumerate(
-            emailMasterContent[application["propertyName"]]
-        ):
-            isSendEmail = True
-            print("emailData = ", emailData)
-            print(emailMasterContent[application["propertyName"]][emailData])
-            if index % 2:
-                table += (
-                    '<tr><td style="border: 1px solid #dddddd; text-align: left;padding: 8px;">'
-                    + emailData
-                    + '</td><td style="border: 1px solid #dddddd; text-align: left;padding: 8px;">'
-                    + str(int(emailMasterContent[application["propertyName"]][emailData]['actual']))
-                    + '</td><td style="border: 1px solid #dddddd; text-align: left;padding: 8px;">'
-                    + str(int(emailMasterContent[application["propertyName"]][emailData]['current']))
-                    + "</td></tr>"
-                )
-            else:
-                table += (
-                    '<tr style="background-color: #dddddd"><td style="border: 1px solid #dddddd; text-align: left;padding: 8px;">'
-                    + emailData
-                    + '</td><td style="border: 1px solid #dddddd; text-align: left;padding: 8px;">'
-                    + str(int(emailMasterContent[application["propertyName"]][emailData]['actual']))
-                    + '</td><td style="border: 1px solid #dddddd; text-align: left;padding: 8px;">'
-                    + str(int(emailMasterContent[application["propertyName"]][emailData]['current']))
-                    + "</td></tr>"
-                )
-        tablehtml += (
+        if application["propertyName"] in emailMasterContent.keys():
+            print("emailMasterContent[application[propertyName] = ", emailMasterContent[application["propertyName"]])
+            for index, emailData in enumerate(
+                emailMasterContent[application["propertyName"]]
+            ):
+                isSendEmail = True
+                print("emailData = ", emailData)
+                print(emailMasterContent[application["propertyName"]][emailData])
+                if index % 2:
+                    table += (
+                        '<tr><td style="border: 1px solid #dddddd; text-align: left;padding: 8px;">'
+                        + emailData
+                        + '</td><td style="border: 1px solid #dddddd; text-align: left;padding: 8px;">'
+                        + str(int(emailMasterContent[application["propertyName"]][emailData]['actual']))
+                        + '</td><td style="border: 1px solid #dddddd; text-align: left;padding: 8px;">'
+                        + str(int(emailMasterContent[application["propertyName"]][emailData]['current']))
+                        + "</td></tr>"
+                    )
+                else:
+                    table += (
+                        '<tr style="background-color: #dddddd"><td style="border: 1px solid #dddddd; text-align: left;padding: 8px;">'
+                        + emailData
+                        + '</td><td style="border: 1px solid #dddddd; text-align: left;padding: 8px;">'
+                        + str(int(emailMasterContent[application["propertyName"]][emailData]['actual']))
+                        + '</td><td style="border: 1px solid #dddddd; text-align: left;padding: 8px;">'
+                        + str(int(emailMasterContent[application["propertyName"]][emailData]['current']))
+                        + "</td></tr>"
+                    )
+            tablehtml += (
+                """
+                <p>Please find the violations Pages in : """
+                + application["propertyName"]
+                + """</p>
+                <table style="font-family: arial, sans-serif; border-collapse: collapse;width: 100%;">
+                    <thead>
+                        <tr>
+                            <td style="border: 1px solid #dddddd; text-align: left;padding: 8px;">Name</td>
+                            <td style="border: 1px solid #dddddd; text-align: left;padding: 8px;">Threshold</td>
+                            <td style="border: 1px solid #dddddd; text-align: left;padding: 8px;">ActualCount</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        """
+                + table
+                + """
+                    </tbody>
+                </table> """
+            )
+            html = (
             """
-            <p>Please find the violations Pages in : """
-            + application["propertyName"]
-            + """</p>
-            <table style="font-family: arial, sans-serif; border-collapse: collapse;width: 100%;">
-                <thead>
-                    <tr>
-                        <td style="border: 1px solid #dddddd; text-align: left;padding: 8px;">Name</td>
-                        <td style="border: 1px solid #dddddd; text-align: left;padding: 8px;">Threshold</td>
-                        <td style="border: 1px solid #dddddd; text-align: left;padding: 8px;">ActualCount</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    """
-            + table
+            <html><body><p>Hello, Team.</p>
+            """
+            + tablehtml
             + """
-                </tbody>
-            </table> """
-        )
-    html = (
-        """
-        <html><body><p>Hello, Team.</p>
-        """
-        + tablehtml
-        + """
-        <p>Regards,</p>
-        <p>Automation Tool</p>
-        </body></html>
-    """
-    )
-    if isSendEmail:
-        _send_email(html)
-    else:
-        print("isSendEmail = False Emai not triggered")
+            <p>Regards,</p>
+            <p>Automation Tool</p>
+            </body></html>
+            """
+            )
+            print("application[propertyName] = ", application["emailConfig"])
+            day = datetime.now().strftime("%A")
+            for emailConfigDays in application["emailConfig"]:
+                print("emailConfigDays = ", emailConfigDays)
+                if day.lower() in emailConfigDays.lower():
+                    print("application[emailConfig] = ", application["emailConfig"][emailConfigDays])
+                    if isSendEmail:
+                        _send_email(html, application["propertyName"], application["emailConfig"][emailConfigDays])
+                    else:
+                        print("isSendEmail = False Emai not triggered")
+
     print("End of _draft_email <<<")
 
 
